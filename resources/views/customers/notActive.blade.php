3b89b40ccf3180ab9fe02ff8138bb770b2566c23
Navid <navid_hero_1st@yahoo.com>
2021-03-26 21:53:23 +0430

    finalize

--- resources/views/customers/notActive.blade.php
+++ resources/views/customers/notActive.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.customer', ['pageTitle' => 'سفارش', 'darkBackground' => true, 'withMenu' => true])
 @section('content')
+@include('layouts.bottomMenu')
 <div  style="text-align:center;color:white;margin-top:150px;">
     <h3>در حال حاضر امکان سفارش گیری</h3>
     <h3>وجود ندارد</h3>
