296a190d79d4e6ce7051021ef5846b1fdc0726bc
herogroup <navid_hero_1st@yahoo.com>
2021-05-05 10:01:12 +0430

    payback script

--- scripts/payback.php
+++ scripts/payback.php
@@ -2,7 +2,7 @@
 
 try {
     echo "script started...".PHP_EOL;
-    $conn = new mysqli("127.0.0.1", "root", "root", "hamsod", 8889);
+    $conn = new mysqli("127.0.0.1", "root", "", "hamsod");
 
     // Check connection
     if ($conn->connect_error)
@@ -13,39 +13,40 @@ try {
         while($product = $products->fetch_assoc()) {
             $productId = $product["id"];
             $realPrice = $product["price"];
-            $maximumMembers = $product["maximum_group_members"];
-            $totalBuyers = 0;
-            $maxDiscount = 0;
-            $lastDiscount = 0;
+            if($productId > 0 && $realPrice > 0) {
+                $maximumMembers = $product["maximum_group_members"];
+                $totalBuyers = 0;
+                $maxDiscount = 0;
+                $lastDiscount = 0;
 
-            $orderItems = $conn->query("SELECT MAX(order_items.nth_buyer) AS total_buyers FROM order_items,orders WHERE order_items.order_id=orders.id AND orders.status=1 AND order_items.available_product_id=$productId;") or die($conn->error);
-            while($orderItem = $orderItems->fetch_assoc()) {
-                $totalBuyers = $orderItem['total_buyers'];
+                $orderItems = $conn->query("SELECT MAX(order_items.nth_buyer) AS total_buyers FROM order_items,orders WHERE order_items.order_id=orders.id AND orders.status=1 AND order_items.available_product_id=$productId;") or die($conn->error);
+                while ($orderItem = $orderItems->fetch_assoc()) {
+                    $totalBuyers = $orderItem['total_buyers'] > 0 ? $orderItem['total_buyers'] : 0;
+                }
+
+                $numberOfCompletedGroups = intdiv($totalBuyers, $maximumMembers);
+                $discounts = $conn->query("SELECT MAX(CONVERT(discount,DECIMAL)) AS max_discount FROM available_product_details WHERE available_product_id=$productId") or die($conn->error);
+                while ($discount = $discounts->fetch_assoc()) {
+                    $maxDiscount = $discount['max_discount'];
+                }
+
+                $level = $totalBuyers - ($numberOfCompletedGroups * $maximumMembers);
+                $discounts = $conn->query("SELECT discount FROM available_product_details WHERE available_product_id=$productId AND level=$level") or die($conn->error);
+                while ($discount = $discounts->fetch_assoc()) {
+                    $lastDiscount = $discount['discount'];
+                }
+
+                $x = $numberOfCompletedGroups * $maximumMembers;
+
+                $max_discount_amount = ($maxDiscount * $realPrice) / 100;
+                $sql = "UPDATE order_items SET extra_discount=$max_discount_amount-discount WHERE available_product_id=$productId AND nth_buyer <= $x";
+                echo $sql . PHP_EOL; // $update = $conn->query(sql) or die($conn->error);
+
+                $last_discount_amount = ($lastDiscount * $realPrice) / 100;
+                $sql = "UPDATE order_items SET extra_discount=$last_discount_amount-discount WHERE available_product_id=$productId AND nth_buyer > $x";
+                echo $sql . PHP_EOL; // $update = $conn->query(sql) or die($conn->error);
             }
-
-            $numberOfCompletedGroups = intdiv($totalBuyers,$maximumMembers);
-            $discounts = $conn->query("SELECT MAX(`discount`) AS max_discount FROM `available_product_details` WHERE `available_product_id`=$productId");
-            while($discount = $discounts->fetch_assoc()) {
-                $maxDiscount = $discount['max_discount'];
-            }
-
-            $level = $totalBuyers - ($numberOfCompletedGroups*$maximumMembers);
-            $discounts = $conn->query("SELECT `discount` FROM `available_product_details` WHERE `available_product_id`=$productId AND `level`=$level");
-            while($discount = $discounts->fetch_assoc()) {
-                $lastDiscount = $discount['discount'];
-            }
-
-            $x = $numberOfCompletedGroups*$maximumMembers;
-
-            $max_discount_amount = ($maxDiscount*$real_price);
-            $sql = "UPDATE `order_items` SET `extra_discount`=$max_discount_amount-`discount` WHERE `available_product_id`=$productId AND `nth_buyer` <= $x";
-            echo $sql.PHP_EOL; // $update = $conn->query(sql);
-
-            $last_discount_amount = ($lastDiscount*$real_price);
-            $sql = "UPDATE `order_items` SET `extra_discount`=$last_discount_amount-`discount` WHERE `available_product_id`=$productId AND `nth_buyer` > $x";
-            echo $sql.PHP_EOL; // $update = $conn->query(sql);
         }
-
     }
 } catch (Exception $exception) { // log exception
     echo $exception->getMessage().PHP_EOL;
