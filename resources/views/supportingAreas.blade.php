32382c25d4159cad2528e333f1ec815f1d7b55a0
Navid Hero <navid_hero_1st@yahoo.com>
2021-05-17 09:05:25 +0430

    fix map and styles for supporting areas

--- resources/views/supportingAreas.blade.php
+++ resources/views/supportingAreas.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.customer', ['pageTitle' => 'مناطق تحت پوشش', 'withNavigation' => true, 'backUrl' => '/' ])
 @section('content')
 
-<div class="container" style="margin-top:70px;color:#222;">
+<div class="container" style="margin:70px 0;color:#222;">
     <h4>مناطق تحت پوشش سرویس دهی همسود</h4>
     <hr>
     <ul>
@@ -10,7 +10,9 @@
     @endforeach
     </ul>
 
-    <img src="/images/supporting_areas.png" height="400" style="width:100%;max-width:400px;" />
+    <a href="/images/supporting_areas.png" target="_blank">
+        <img src="/images/supporting_areas.png" height="400" style="width:100%;max-width:400px;" />
+    </a>
 </div>
 
 @endsection
